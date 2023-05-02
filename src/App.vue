a4b6b9ef7a234048d5b132b93aaf258ac7f4ea08
Luis-Mocha <luismocha97@gmail.com>
2023-05-02 18:56:17 +0200

    componente AppTitle (export not working)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue';
+
+// export default{
+//     name: "App",
+//     components: {
+//       AppTitle
+//     }
+// }
+
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,20 +19,14 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
+
+  <h1>Ciao a tutti</h1>
+
+  <AppTitle/>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script setup>
+    // export default{
+    //     name: "AppTitle",
+    //     // data (){
+    //     //     return()
+    //     // },
+    //     // methods: {
+
+    //     // }
+    // }
+</script>
+
+<template>
+    <h2>Questo è un h2</h2>
+    <span>Questo è un elemento span</span>
+</template>
+
+<style scoped>
+    h2 {
+        color: red;
+    }
+</style>
